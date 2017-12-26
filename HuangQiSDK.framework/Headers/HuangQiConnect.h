be222669bac1774475ede5bfd6d06ff88e93bcf4
joneyin <yin_jone@163.com>
2017-12-26 15:47:56 +0800

    Update

--- HuangQiSDK.framework/Headers/HuangQiConnect.h
+++ HuangQiSDK.framework/Headers/HuangQiConnect.h
@@ -2,19 +2,16 @@
 //  HuangQiConnect.h
 //  HuangQiSDK
 //
-//  Created by Jone Yin on 2017/11/10.
+//  Created by Jone Yin on 2017/12/26.
 //  Copyright © 2017年 Gotell. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @import FonSDKConnect;
-@import WisprSDK;
-
-@interface HuangQiConnect : NSObject<FonSDKConnectDelegate>
-
--(void)start:(NSString *)data;
--(void)stop;
--(void)reset;
-
+@interface HuangQiConnect : NSObject
+@property (strong, nonatomic) FonSDKConnect *fonConnect;
+- (void)start:(NSString *)data bundleldenfitier:(NSString *)idenfitier;
+- (void)stop;
+- (void)reset;
 @end
